3b76b898ff9c6bcca6ed347ccf1954f2c8947bef
Deep Red <0k5786@gmail.com>
2017-09-01 23:43:36 -0400

    Cart Creation.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    session[:counter].nil? ? session[:counter] = 0 : session[:counter] += 1
+    @counter = session[:counter]
   end
 end
