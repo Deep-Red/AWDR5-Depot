074a32ba21ac7838b0b955063df85be96cc79d51
Deep Red <0k5786@gmail.com>
2017-09-07 08:41:34 -0400

    Logging in.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,8 @@
 class StoreController < ApplicationController
+  skip_before_action :authorize
   include CurrentCart
   before_action :set_cart
-  
+
   def index
     @products = Product.order(:title)
     session[:counter].nil? ? session[:counter] = 0 : session[:counter] += 1
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :users
   resources :orders
+
   resources :line_items do
     post 'decrement'
   end
+
   resources :carts
   root 'store#index', as: 'store_index'
 
